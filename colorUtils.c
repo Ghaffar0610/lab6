7f108a874e10ace66f7cebff7823a2eb0bc2d679
Ghaffar <ghaffarbwp786@gmail.com>
2023-12-22 00:42:14 +0500

    average ,lightness and luminosity techniques added in utils.c

--- colorUtils.c
+++ colorUtils.c
@@ -11,8 +11,25 @@ int min(int a, int b, int c) {
   return (a < b) ? (a < c ? a : c) : (b < c ? b : c);
 }
 
+// This function return Error and the Value of diferent filter
+Error toGrayScale(int *r, int *g, int *b, Mode mode) {
+  // if the address or refrence pass to this function as 0 it will return the value 1
+  if(r == NULL || g == NULL || b == NULL ){
+    return ERROR_PRESENT;
+  }}
 int toGrayScale(int *r, int *g, int *b, Mode mode) {
   //TODO: implement
+   if (mode == AVERAGE) {
+    *r = *g = *b = (*r + *g + *b) / 3;
+  }
+  //FOR LIGHTNESS TECHNIQUE
+  else if (mode == LIGHTNESS) {
+    *r = *g = *b = (max(*r, *g, *b) + min(*r, *g, *b))/2;
+  } 
+  //FOR LUMINOSITY TECHNIQUE
+  else if (mode == LUMINOSITY) {
+    *r = *g = *b = round(0.21 * *r + 0.72 * *g + 0.07 * *b);
+  }
 }
 
 int toSepia(int *r, int *g, int *b) {
